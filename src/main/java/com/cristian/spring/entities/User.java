c9da2ed0fccd1581a9a6092f9c83e2d912f9791a
Cristian Eduardo dos Santos <cristian.santos0308@gmail.com>
2023-03-20 20:50:31 -0300

    Order entity

--- src/main/java/com/cristian/spring/entities/User.java
+++ src/main/java/com/cristian/spring/entities/User.java
@@ -1,11 +1,14 @@
 package com.cristian.spring.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -25,6 +28,9 @@ public class User implements Serializable{
 	private String phone;
 	
 	private String password;
+	
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
 
 	public User() {
 	}
@@ -37,6 +43,7 @@ public class User implements Serializable{
 		this.phone = phone;
 		this.password = password;
 	}
+	
 
 	public Long getId() {
 		return id;
@@ -77,6 +84,10 @@ public class User implements Serializable{
 	public void setPassword(String password) {
 		this.password = password;
 	}
+	
+	public List<Order> getOrders() {
+		return orders;
+	}
 
 	@Override
 	public int hashCode() {
@@ -102,6 +113,8 @@ public class User implements Serializable{
 			return false;
 		return true;
 	}
+
+	
 	
 	
 
